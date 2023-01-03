5373b96f52a292d481b6b184af4b8cb6e81b86d1
Kevin Coto <67404478+KevinCotoCarrera@users.noreply.github.com>
2023-01-03 17:09:37 -0500

    fix: apply linter suggestions

--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  HttpException,
-  HttpStatus,
-  Injectable,
-} from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../model/user.schema';
