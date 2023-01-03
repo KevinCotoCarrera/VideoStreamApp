2007bbdbc58ba1aa3a1451994d00e88c3dece9f2
Kevin Coto <67404478+KevinCotoCarrera@users.noreply.github.com>
2023-01-03 12:01:08 -0500

    feat: Creates User Controller

---
+++ backend/src/controller/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { User } from '../model/user.schema';
+import { UserService } from '../service/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Controller('/api/v1/user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+  @Post('/signup')
+  async SignUp(@Res() response, @Body() user: User) {
+    const newUser = await this.userService.signup(user);
+    return response.status(HttpStatus.CREATED).json({ newUser });
+  }
+  @Post('/signin')
+  async SignIn(@Res() response, @Body() user: User) {
+    const token = await this.userService.signin(user, this.jwtService);
+    return response.status(HttpStatus.OK).json(token);
+  }
+}
