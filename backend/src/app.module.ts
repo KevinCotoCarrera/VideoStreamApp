50993ff4c4de6210768ee5d31909df7a747c56df
Kevin Coto <67404478+KevinCotoCarrera@users.noreply.github.com>
2023-01-03 11:14:09 -0500

    feat: Establish connection to MongoDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Stream')],
   controllers: [AppController],
   providers: [AppService],
 })
