735fb236d824b66b0cbf4ccf301529b925dca174
Kevin Coto <67404478+KevinCotoCarrera@users.noreply.github.com>
2023-01-03 12:00:58 -0500

    feat: Add Jwt Register, ServerStatic and Multir

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Stream')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
