283ca4e84f4d9fad2b9fb2fec8f83a5ad77bc83d
Kevin Coto <67404478+KevinCotoCarrera@users.noreply.github.com>
2023-01-05 06:09:28 -0500

    fix: packages bugs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { secret } from './utils/constants';
-import { join } from 'path/posix';
+import { join } from 'path';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
